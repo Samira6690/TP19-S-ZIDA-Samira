e1637787df0bf2b8877b8a904683e15923132dc1
Samira6690 <zidasamiratou34@gmail.com>
2023-08-04 15:18:00 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// const button = document.querySelector(".button");
+// const toi  = document.querySelector(".toi");
+// const moi = document.querySelector(".moi");
+// const soi = document.querySelector(".soi");
+// const a = document.querySelector(".a");
+// button.addEventListener("click", () => {
+//     moi.style.display = "flex";
+//     console.log("rue");
+//     toi.style.display = "none";
+//     soi.style.display = "none";
+// });
+// a.addEventListener("click", () =>{
+//     soi.style.display = "none";
+//     console.log("rue");
+//     toi.style.display="none";
+//     moi.style.display = "none";
+// });
